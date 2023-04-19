dcc63e6d5703956afa2d061bb9a577cfd1e6869c
KaiOWhatmore <oscar1995@live.com.au>
2023-04-20 09:53:02 +1000

    include react-router-dom to enable mutlipage design

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Switch, RouterProvider } from 'react-router-dom';
+import LandingPage from './LandingPage';
+import ChartsPage from './ChartsPage';
 import LineChart from './Charts/LineChart';
 import BarChart from './Charts/BarChart';
 import BarChartAmountPerMonth from './Charts/BarChartAmountPerMonth'
@@ -8,9 +11,13 @@ function App() {
 
   return (
     <div className="App">
-      {/* <LineChart /> */}
       {/* <BarChart /> */}
-      <BarChartAmountPerMonth />
+      {/* <LineChart />  */}
+      <Routes>
+        {/* <Route path="/barchart" element={<BarChart />} />   */}
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/charts" element={<ChartsPage />} />
+      </Routes>
     </div>
   );
 }
