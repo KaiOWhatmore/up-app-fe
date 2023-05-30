9429e9c2bc8ec2bdd4b5c57eaeaac89d9511ce5e
KaiOWhatmore <oscar1995@live.com.au>
2023-05-30 22:51:25 +1000

    it's finally all coming together

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,8 @@ import LineChart from './Charts/LineChart';
 import BarChart from './Charts/BarChart';
 import BarChartAmountPerMonth from './Charts/BarChartAmountPerMonth'
 import NavBar from "./NavBar";
+import LineChartRechart from "./charts-rechart/LineChartRechart"
+import LineGraphZoom from "./charts-rechart/LineGraphZoom"
 
 function App() {
 
@@ -16,12 +18,14 @@ function App() {
     {/* // <div className="App"> */}
         <NavBar/>
         <Routes>
+          <Route path="/chart-re" element={<LineChartRechart/>} />
           <Route path="/" element={<LandingPage />} />
           <Route path="/charts" element={<ChartsPage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/barcharts" element={<BarChart />} />
           <Route path="/graphs" element={<LineChart />} />
           <Route path="/barchartspermonth" element={<BarChartAmountPerMonth />} />
+          <Route path="/charts-zoom" element={[<LineGraphZoom/>]}/>
         </Routes>
         {/* </div> */}
     </Router>
--- client/src/Charts/LineChart.js
+++ client/src/Charts/LineChart.js
@@ -29,7 +29,7 @@ ChartJS.register(
 const LineChart = () => {
 
   const [chart, setChart] = useState({ data: [] })
-  let proxyUrl = "/api/transactions";
+  let proxyUrl = "/api/transactions/curt/runningTotal";
 
   useEffect(() => {
     const fetchTransactions = async () => {
@@ -43,11 +43,6 @@ const LineChart = () => {
         .then((response) => {
           if (response.ok) {
             response.json().then((json) => {
-              let totalAmount = 0;
-              json.forEach(element => {
-                totalAmount += Number(element.amount);
-                element.total = totalAmount;
-              })
               setChart({ data: json });
             });
           }
@@ -62,24 +57,31 @@ const LineChart = () => {
     labels: chart?.data?.map(x => x.settledAt),
     datasets: [{
       label: `${chart?.data?.length} Transactions Present`,
-      data: chart?.data?.map(x => x.total),
+      data: chart?.data?.map(x => x.runningTotalDouble),
       backgroundColor: 'rgba(0, 0, 0, 0.5)',
       borderColor: 'rgb(128, 0, 0, 0.5)',
-      borderWidth: 0.5
+      borderWidth: 1,
+      tension: 0.5
     }]
   };
 
-  let options = {
+  const options = {
+    maintainAspectRatio: false,
+    responsive: true,
+    scales: {
+      x: {
+        beginAtZero: true,
+      },
+      y: {
+        beginAtZero: true,
+      },
+    },
     plugins: {
       zoom: {
         pan: {
           enabled: true,
           mode: 'xy'
         },
-        limits: {
-          x: 35000
-        
-        }, 
         zoom: {
           wheel: {
             enabled: true,
@@ -87,45 +89,32 @@ const LineChart = () => {
           pinch: {
             enabled: true,
           },
-          mode: 'xy',
-          minScale: 1, 
-          maxScale: 5
+          mode: 'x',
         }
       }
-    },
-    maintainAspectRatio: false,
-    scales: {
-      // y: {
-      //   min: 0,
-      //   max: 10000
-      // },
-      // x: {
-      //   ticks: {
-      //     font: {
-      //       size: 18
-      //     },
-      //     maxRotation: 90,
-      //     minRotation: 90
-      //   },
-      //   grid: {
-      //     display: false
-      //   }
-      // }
     },
     legend: {
       labels: {
         fontSize: 25,
       },
     },
+    elements: {
+      point: {
+        radius: 0
+      }
+    }
   }
 
   return (
     <div className="chart-container">
-      <Line
-        data={data}
-        height={null}
-        options={options}
-      />
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '90vh' }}>
+        <div style={{ width: '1600px', height: '700px' }}>
+          <Line
+            data={data}
+            options={options}
+          />
+        </div>
+      </div>
     </div>
   )
 }
