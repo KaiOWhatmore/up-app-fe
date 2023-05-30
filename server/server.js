a132bbdc8aa1794351a7f21b70e33ef687ae0246
KaiOWhatmore <oscar1995@live.com.au>
2023-05-30 22:52:36 +1000

    bunch of utils shit from external libraries

--- server/server.js
+++ server/server.js
@@ -6,51 +6,15 @@ const port = 3001;
 
 app.use(cors()) 
 
-app.get('/api/transactions', (req, res) => {
-    const request = unirest("GET", "http://localhost:8080/transactions");
+app.get('/api/transactions/curt/runningTotal', (req, res) => {
+    const request = unirest("GET", "http://localhost:8080/transactions/curt/runningTotal");
     request.end(function (response) {
         if (response.error) throw new Error(response.error);
 
         res.json(response.body || {});
     })
   });
-
-app.get('/api/transactions/categories', (req, res) => {
-  const request = unirest("GET", "http://localhost:8080/transactions/categories");
-  request.end(function (response) {
-      if (response.error) throw new Error(response.error);
-
-      res.json(response.body || {});
-  })
-});
-
-app.get('/api/transactions/monthly-savings', (req, res) => {
-  const request = unirest("GET", "http://localhost:8080/transactions/monthly-savings");
-  request.end(function (response) {
-      if (response.error) throw new Error(response.error);
-
-      res.json(response.body || {});
-  })
-});
-
-app.get('/api/transactions/monthly-income', (req, res) => {
-  const request = unirest("GET", "http://localhost:8080/transactions/monthly-income");
-  request.end(function (response) {
-      if (response.error) throw new Error(response.error);
-
-      res.json(response.body || {});
-  })
-});
-
-app.get('/api/transactions/monthly-expenses', (req, res) => {
-  const request = unirest("GET", "http://localhost:8080/transactions/monthly-expenses");
-  request.end(function (response) {
-      if (response.error) throw new Error(response.error);
-
-      res.json(response.body || {});
-  })
-});
-
+  
 app.listen(port, () => {
 console.log(`Example app listening at http://localhost:${port}`)
 })
